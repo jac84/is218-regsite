136ea29ba73cfce839790c64df9fd7dda1f3fc72
Grace A Young <gay5@afsconnect1.njit.edu>
2016-10-17 22:56:52 -0400

    form

--- form.php
+++ form.php
@@ -5,54 +5,60 @@
 </head>
 
 <body>
+
 <form action = "form.php" method = "post">
 	<div>
 		<center>
+		<h1> Sign up today! </h1> <br> <br> <br>
+		</center>
+	</div>
+	<div>
+		<center>
 		<label for = "name"> Name: </label> <br>
-		<input type = "text" id = "name" name = "user_name" /> <br>
+		<input type = "text" id = "name" name = "user_name" /> <br> <br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<label for = "mail" > E-mail: </label> <br>
-		<input type = "email" id = "mail" name = "user_mail" /> <br>
+		<input type = "email" id = "mail" name = "user_mail" /> <br> <br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<label for = “username”> Username: </label> <br>
-		<input type = “text” id = “username” name = “user_username” /> <br>
+		<input type = “text” id = “username” name = “user_username” /> <br> <br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<label for = “age”> Age: </label> <br>
-		<input type = “text” id = “age” name = “user_age” /> <br>
+		<input type = “text” id = “age” name = “user_age” /> <br> <br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<label for = “gender”> Gender: </label> <br>
-		<input type = “radio” name = "gender" value = "male" Male /> <br>
-		<input type = "radio" name = "gender" value = "female" Female /> <br>
+		<input type = “text” id = "gender" name = user_gender /> <br> <br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<label for = “password”> Password: </label> <br>
-		<input type = “text” id = “password” name = “user_password” /> <br>
+		<input type = “text” id = “password” name = “user_password” /> <br> <br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<label for = “passwordconf”> Confirm Passowrd: </label> <br>
 		<input type = “text” id = “passwordconf” name = “user_passwordconf” /> <br>
+		<br>
 		</center>
 	</div>
 	<div>
 		<center>
 		<div class = "button"> <br>
-		<button type = "submit">Thank you for registering!</button>
+		<button type = "submit">Submit</button>
 		</center>
 	</div>
 </body>
