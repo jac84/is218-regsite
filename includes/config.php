c20c80adf70767e6c5e4d563e9fd43b3eca71dad
Jennifer A Causing <jac84@afsconnect2.njit.edu>
2016-12-20 09:54:54 -0500

    finally added User table

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+
+ob_start();
+session_start();
+
+date_default_timezone_set('North America/New York');
+
+define('DBHOST', 'localhost');
+define('DBUSER', 'database user');
+define('DBPASS', 'password');
+define('DBNAME', 'database name');
+
+define('DIR','');
+define('SITEEMAIL', '');
+
+
+
+?>
