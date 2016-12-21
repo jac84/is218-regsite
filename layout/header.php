59966b02cb7432d666c82a150f9430e2e88d8512
Jennifer A Causing <jac84@afsconnect2.njit.edu>
2016-12-21 02:36:17 -0500

    created html header and footer php files and css stylesheet

---
+++ layout/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML>
+
+<head>
+    <title><?php if(isset($title)){ echo $title; }?></title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
