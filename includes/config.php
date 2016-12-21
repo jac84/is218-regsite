44a49e49092a68bb6f814b77a2a8e72204e1f3f6
Jennifer A Causing <jac84@afsconnect2.njit.edu>
2016-12-21 01:52:32 -0500

    started config.php flie

--- includes/config.php
+++ includes/config.php
@@ -3,16 +3,21 @@
 ob_start();
 session_start();
 
-date_default_timezone_set('North America/New York');
+date_default_timezone_set('America/New_York'); //set db access timezone
 
-define('DBHOST', 'localhost');
-define('DBUSER', 'database user');
-define('DBPASS', 'password');
-define('DBNAME', 'database name');
+/*database credentials*/
+define('DBHOST','sql1.njit.edu');
+define('DBUSER','jac84');
+define('DBPASS','Aa9v010j');
+define('DBNAME','jac84');
 
-define('DIR','');
-define('SITEEMAIL', '');
-
-
+try {
+	$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "pdo connection successful";
+} catch(PDOException $e) {
+    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+    exit;
+}
 
 ?>
